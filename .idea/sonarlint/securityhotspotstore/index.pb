dbe3975566c3d93c628d2055f4f2db3028f10e63
Jardel <jardel.gregorio2009@gmail.com>
2024-03-14 16:38:37 -0300

    first commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,11 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+n
+>src/main/java/com/jardel/crudspring/CrudSpringApplication.java,2/1/21407c61717b2b85e73a23342858da6b2008619b
+t
+Dsrc/main/java/com/jardel/crudspring/repository/CourseRepository.java,4/f/4f7b97bc5acf8d2333a48833bdcf78869d2bf2c9
+e
+5src/main/java/com/jardel/crudspring/model/Course.java,5/e/5e2a6417480f7ed9c86751b30e42b6ca834b4ad8
+t
+Dsrc/main/java/com/jardel/crudspring/controller/CourseController.java,a/e/aeb97aa9735d4549ae470f5a4a6b8754c7309c8e
---
+++ src/main/java/com/jardel/crudspring/controller/CourseController.java
@@ -0,0 +1,59 @@
+package com.jardel.crudspring.controller;
+
+import com.jardel.crudspring.model.Course;
+import com.jardel.crudspring.repository.CourseRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+    private final CourseRepository courseRepository;
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course ) {
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    recordFound.setName(course.getName());
+                    recordFound.setCategory(course.getCategory());
+                    Course updated = courseRepository.save(recordFound);
+                    return ResponseEntity.ok().body(updated);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public ResponseEntity<Void> update(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    courseRepository.deleteById(id);
+                    return ResponseEntity.noContent().<Void>build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
